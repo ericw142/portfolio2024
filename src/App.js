41a857c895f09d07f6b299fd4f584bc4eae940c0
ericw142 <eric.h.warshawsky@gmail.com>
2024-01-04 11:06:17 -0500

    basic header structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './eric_w.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="brand-header">
+        <div className="logo">
+            <img src={logo} alt="eric-warshawsky" />
+        </div>
+        <div className="brand-name">
+            <p className="font-32">Eric Warshawsky</p>
+            <p className="font-16">Full-Stack Web Developer</p>
+        </div>
       </header>
     </div>
   );
