4ac2740f53b43dd79cd9d7297ee37a3ff743ed4a
ericw142 <eric.h.warshawsky@gmail.com>
2024-01-08 13:35:48 -0500

    linked in added in

--- src/components/BrandHeader.js
+++ src/components/BrandHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../eric_w.jpeg';
-
+import { FaLinkedin } from "react-icons/fa";
 
 export const BrandHeader = () => {
     return (
         <div>
+            <div><a href="https://www.linkedin.com/in/eric-warshawsky-94a96b160/" target="_blank" className="linked-in font-32"><FaLinkedin /></a></div>
             <div id="brand-header">
                 <div className="logo">
                     <img src={logo} alt="eric-warshawsky" />
