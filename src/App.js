1045ff8d4690ea97fe366c4d4154ff45de261db0
ericw142 <eric.h.warshawsky@gmail.com>
2024-01-10 14:24:06 -0500

    spacing

--- src/App.js
+++ src/App.js
@@ -6,52 +6,52 @@ import { About } from './components/About';
 import { CanvasAnimation } from './components/CanvasAnimation';
 
 function App() {
-  const [isMobile, setIsMobile] = useState(false)
- 
-  const handleResize = () => {
-    if (window.innerWidth < 720) {
-        setIsMobile(true)
-    } else {
-        setIsMobile(false)
+    const [isMobile, setIsMobile] = useState(false);
+    
+    const handleResize = () => {
+        if (window.innerWidth < 720) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
     }
-  }
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize)
-  })
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+    })
 
-  useEffect(() => {
-    handleResize()
-  }, [])
+    useEffect(() => {
+        handleResize()
+    }, [])
 
-  return (
-    <div className="App">
-      <CanvasAnimation />
-      {isMobile ? (
-        <div id="brand-wrapper">
-          <div className="flex-col">
-            <BrandHeader />
-            <About />
-            <Portfolio />
-          </div>
-        </div>
-      ) : (
-        <div id="brand-wrapper">
-            <div className="padding">
-              <div className="flex-row">
-                <div className="flex-col">
-                <Portfolio />
-                <About />
-                </div>
+    return (
+        <div className="App">
+            <CanvasAnimation />
+            {isMobile ? (
+                <div id="brand-wrapper">
                 <div className="flex-col">
                     <BrandHeader />
+                    <About />
+                    <Portfolio />
                 </div>
-              </div>
-            </div>
+                </div>
+            ) : (
+                <div id="brand-wrapper">
+                    <div className="padding">
+                    <div className="flex-row">
+                        <div className="flex-col">
+                        <Portfolio />
+                        <About />
+                        </div>
+                        <div className="flex-col">
+                            <BrandHeader />
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,10 +5,10 @@ export const Portfolio = () => {
         <div id="project-links">
             <h2>Projects</h2>
             <div className="flex-col">
-            <a className="project-link" href="#">e-commerce</a>
-            <a className="project-link" href="#">coming soon...</a>
-            <a className="project-link" href="#">coming soon...</a>
-            <a className="project-link" href="#">coming soon...</a>
+                <a className="project-link" href="#">e-commerce</a>
+                <a className="project-link" href="#">coming soon...</a>
+                <a className="project-link" href="#">coming soon...</a>
+                <a className="project-link" href="#">coming soon...</a>
             </div>
         </div>
     )
