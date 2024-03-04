d914c626034cd55414ce8a3b5e93568a944cbaba
ericw142 <eric.h.warshawsky@gmail.com>
2024-03-04 10:27:12 -0500

    updated project links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,8 +5,8 @@ export const Portfolio = () => {
         <div id="project-links">
             <h2>Projects</h2>
             <div className="flex-col">
-                <a className="project-link" href="https://ericw142.github.io/ecommerce/" target="_blank">e-commerce</a>
-                <a className="project-link" href="#">coming soon...</a>
+                <a className="project-link" href="https://ericw142.github.io/ecommerce/" target="_blank" rel="noreferrer">e-commerce</a>
+                <a className="project-link" href="https://ericw142.github.io/netflix_clone/" target="_blank" rel="noreferrer">netflix clone</a>
                 <a className="project-link" href="#">coming soon...</a>
                 <a className="project-link" href="#">coming soon...</a>
             </div>
