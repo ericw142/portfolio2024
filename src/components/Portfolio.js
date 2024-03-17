e1a8a6ddfdc28b63400754148bce16ffc728f7fe
ericw142 <eric.h.warshawsky@gmail.com>
2024-03-17 14:55:19 -0400

    link fix

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,7 @@ export const Portfolio = () => {
         <div id="project-links">
             <h2>Projects</h2>
             <div className="flex-col">
-                <a className="project-link" href="https://my-video-games-list-gbt9i68a1-ericw142s-projects.vercel.app/" target="_blank" rel="noreferrer">MyVideoGamesList</a>
+                <a className="project-link" href="https://my-video-games-list.vercel.app/" target="_blank" rel="noreferrer">MyVideoGamesList</a>
                 <a className="project-link" href="https://ericw142.github.io/ecommerce/" target="_blank" rel="noreferrer">e-commerce</a>
                 <a className="project-link" href="https://ericw142.github.io/netflix_clone/" target="_blank" rel="noreferrer">netflix clone</a>
                 <a className="project-link" href="#">coming soon...</a>
